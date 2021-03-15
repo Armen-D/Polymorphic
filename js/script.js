ef91acdd0493943745131b74fae778974fbd2bbd
Armen Barseghyan <armenbarseghyan10@gmail.com>
2021-03-15 22:52:06 +0400

    Finish

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 let burger = document.getElementById("burger-menu");
-let sidebar = document.querySelector(".sideBar")
+let sidebar = document.querySelector(".mobile-bar")
 let closeMenu = document.querySelector(".icon-cross")
-const sidemenu = document.querySelectorAll(".side-nav a");
 burger.addEventListener("click", () => {
     sidebar.classList.add("active");
 });
